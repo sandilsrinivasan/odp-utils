779648f7ddd3e55350a46030d97edac34f4b866e
Sandil Srinivasan <>
2018-08-23 01:24:30 +0530

    Fix to handle relationships

--- odp-config-migratie.js
+++ odp-config-migratie.js
@@ -5,11 +5,12 @@ clear();
 
 var readline = require('readline-sync');
 const req = require('request-promise');
+const reqSync = require('sync-request');
 
 var DEFAULT = {
 	port: 32001,
-	sourceHostname: 'sandbox.odp.capiot.com',
-	targetHostname: 'sandbox.odp.capiot.com'
+	sourceHostname: 'bifrost.odp.capiot.com',
+	targetHostname: 'bifrost.odp.capiot.com'
 }
 
 const API = {
@@ -64,6 +65,8 @@ var targetPassword = readline.question('Password: ', {
 CONFIG.targetDomain = readline.question('Domain: ');
 CONFIG.targetToken = null;
 
+var toBeExported = [];
+
 function login(_url, _username, _password) {
 	var options = {
 		method: "POST",
@@ -74,13 +77,13 @@ function login(_url, _username, _password) {
 	    	password: _password
 	    }
 	}
-	return req(options)
+	return req(options);
 }
 
 function getServices(_url, _token, _domain){
 	var options = {
 		method: "GET",
-		uri: _url + API.service,
+		uri: _url + API.service + '?count=-1',
 		headers: {
 			"Content-Type": "application/json",
 			"Authorization": "JWT " + _token
@@ -89,11 +92,11 @@ function getServices(_url, _token, _domain){
 	}
 
 	if (readline.keyInYN('Export only active services? ')) {
-		options.uri += '?filter={"domain":"' + _domain + '","status":"Active"}'
+		options.uri += '&filter={"domain":"' + _domain + '","status":"Active"}'
 	} else {
-		options.uri += '?filter={"domain":"' + _domain + '"}'
+		options.uri += '&filter={"domain":"' + _domain + '"}'
 	}
-	return req(options)
+	return req(options);
 }
 
 function createDomain(_url, _token, _name, _description) {
@@ -111,7 +114,7 @@ function createDomain(_url, _token, _name, _description) {
 	    	"description": _description
 	    }
 	}
-	return req(options)
+	return req(options);
 }
 
 function createService(_url, _token, _service) {
@@ -122,37 +125,134 @@ function createService(_url, _token, _service) {
 			"Content-Type": "application/json",
 			"Authorization": "JWT " + _token
 		},
-	    json: true,
-	    body: _service
+	    json: _service
 	}
-	return req(options)
+	var res = reqSync(options.method, options.uri, options);
+	// console.log(res.getBody('utf8'));;
+	return JSON.parse(res.getBody('utf8'));
+}
+
+function getNewId(_oldId) {
+	var newId = null;
+	// console.log("_oldId = " + _oldId);
+	toBeExported.forEach((service, i) => {
+		// console.log("service._id = " + service._id);
+		// console.log("service.ocmExported = " + service.ocmExported);
+		if(service._id == _oldId && service.ocmExported)
+			newId = service.ocmId;
+	});
+	return newId;
+}
+
+function allDependenciesDone(_service) {
+	var allDone = true;
+	console.log("Checking if all dependencies of " + _service.api + " are done");
+	_service.relatedSchemas.outgoing.forEach((_dep) => {
+		// console.log("Checking: " + _dep.service);
+		toBeExported.forEach((service, i) => {
+			// console.log("Checking against: " + service._id);
+			if(service._id == _dep.service) {
+				if(!service.ocmExported) {
+					allDone = false;
+				}
+			}
+		});
+	});
+	return allDone;
+}
+
+function allServicesDone() {
+	var allDone = true;
+	toBeExported.forEach((service, i) => {
+		if(!service.ocmExported)
+			allDone = false;
+	});
+	return allDone;
 }
 
 function createServices() {
+
 	getServices(CONFIG.sourceUrl, CONFIG.sourceToken, CONFIG.sourceDomain).then(_services => {
 		console.log("Creating services ...");
 		_services.forEach((service, i) => {
-			var s = {};
-			s.api = service.api;
-			s.attributeList = service.attributeList;
-			s.definition = JSON.parse(service.definition);
-			s.description = service.description;
-			s.domain = CONFIG.targetDomain;
-			s.name = service.name;
-			s.preHooks = service.preHooks;
-			s.tags = service.tags;
-			s.version = 1;
-			s.versionValidity = service.versionValidity;
-			s.webHooks = service.webHooks;
-			s.wizard = service.wizard;
-			// dirty hack
-			// because odp misbehaves with roles when hit too quickly
-			setTimeout(function() {
-				createService(CONFIG.targetUrl, CONFIG.targetToken, s).then(_remoteService => {
-					console.log("Created service: " + service.name);
-				});
-			} , ((i+1) * 1000));
-		})
+
+			service.ocmId = "";
+			service.ocmExported = false;
+			toBeExported.push(service);
+
+		});
+
+		do {
+
+			toBeExported.forEach((service, i) => {
+
+				if(!service.ocmExported) {
+
+					var s = {};
+					s.api = service.api;
+					s.attributeList = service.attributeList;
+					s.definition = JSON.parse(service.definition);
+					s.description = service.description;
+					s.domain = CONFIG.targetDomain;
+					s.name = service.name;
+					s.preHooks = service.preHooks;
+					s.tags = service.tags;
+					s.version = 1;
+					s.versionValidity = service.versionValidity;
+					s.webHooks = service.webHooks;
+					s.wizard = service.wizard;
+
+					// Check if this service has any outgoing relationships
+					if(service.relatedSchemas.outgoing && service.relatedSchemas.outgoing.length > 0) {
+						// Check if all dependent services have already been exported
+						if(allDependenciesDone(service)) {
+							// enrich the IDs
+
+							for (var key in s.definition) {
+								// console.log("key = " + key);
+								// console.log("type = " + s.definition[key].type);
+								if(s.definition[key].properties && s.definition[key].properties.relatedTo) {
+									var newId = getNewId(s.definition[key].properties.relatedTo);
+									// console.log("old id = " + s.definition[key].properties.relatedTo + ", new id = " + newId);
+									var props = s.definition[key].properties;
+									s.definition[key] = {};
+									s.definition[key]._newField = false;
+									s.definition[key].properties = props;
+									s.definition[key].properties.relatedTo = newId;
+									s.definition[key].properties._typeChanged = 'Relation';
+									s.definition[key].type = 'Relation';
+								}
+							}
+
+							// safe to export
+							// dirty hack
+							// because odp misbehaves with roles when hit too quickly
+							console.log("Creating service " + service.name + " ... ");
+							// console.log(JSON.stringify(s));
+							var _remoteService = createService(CONFIG.targetUrl, CONFIG.targetToken, s);
+							console.log("Created service: " + service.name + " with id: " + _remoteService._id);
+							service.ocmId = _remoteService._id
+							service.ocmExported = true;
+						} else {
+							console.log("Skipping service: " + service.name + ", not all dependencies have been created ...");
+						}
+
+					} else {
+						// if not, it's safe to export
+						// dirty hack
+						// because odp misbehaves with roles when hit too quickly
+						console.log("Creating service " + service.name + " ... ");
+						var _remoteService = createService(CONFIG.targetUrl, CONFIG.targetToken, s);
+						console.log("Created service: " + service.name + " with id: " + _remoteService._id);
+						service.ocmId = _remoteService._id
+						service.ocmExported = true;
+					}
+
+				}
+
+			});
+
+		} while(!allServicesDone())
 	})
 }
 
